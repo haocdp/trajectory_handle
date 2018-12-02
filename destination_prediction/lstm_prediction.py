ba6898e9d0cc4c87fe4c6b1ce0d98feb1568be1a
haoc_dp <haoc_dp@163.com>
2018-12-02 16:10:54 +0800

    改成 cuda

--- destination_prediction/lstm_prediction.py
+++ destination_prediction/lstm_prediction.py
@@ -7,6 +7,7 @@ torch: 0.4
 matplotlib
 torchvision
 """
+import os
 import torch
 from torch import nn
 import torchvision.datasets as dsets
@@ -17,6 +18,8 @@ import torch.utils.data as Data
 import torch.nn.functional as F
 
 # torch.manual_seed(1)    # reproducible
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"  # gpu
+gpu_avaliable = torch.cuda.is_available()
 
 # Hyper Parameters
 EPOCH = 10  # train the training data n times, to save time, we just train 1 epoch
@@ -25,13 +28,13 @@ TIME_STEP = 10  # rnn time step / image height
 INPUT_SIZE = 30  # rnn input size / image width
 LR = 0.01  # learning rate
 
-labels = list(np.load("F:\FCD data\cluster\destination_labels.npy"))
+labels = list(np.load("/root/data/cluster/destination_labels.npy"))
 # label个数
 label_size = len(set(labels))
 
 
 def load_data():
-    filepath = "F:\FCD data\\trajectory\workday_trajectory_destination\youke_1_result_npy.npy"
+    filepath = "/root/data/trajectory/workday_trajectory_destination/youke_1_result_npy.npy"
     trajectories = list(np.load(filepath))
     count = len(trajectories) * 0.8
 
@@ -51,7 +54,6 @@ def load_data():
                 poi_to_ix[t[-1]] = len(poi_to_ix)
             if t[-2] not in region_to_ix:
                 region_to_ix[t[-2]] = len(region_to_ix)
-
 
     def transfer(tra):
         new_tra = []
@@ -96,14 +98,14 @@ train_data, train_labels, test_data, test_labels, car_to_ix, poi_to_ix = load_da
 
 train_data = torch.FloatTensor(train_data)
 train_labels = torch.LongTensor(train_labels)
-test_data = torch.FloatTensor(test_data)
-test_labels = torch.LongTensor(test_labels).numpy()
+test_data = torch.FloatTensor(test_data).cuda() if gpu_avaliable else torch.FloatTensor(test_data)
+test_labels = torch.LongTensor(test_labels).cuda() if gpu_avaliable else torch.LongTensor(test_labels)
 
 torch_dataset = Data.TensorDataset(train_data, train_labels)
 loader = Data.DataLoader(
-    dataset=torch_dataset,      # torch TensorDataset format
-    batch_size=BATCH_SIZE,      # mini batch size
-    shuffle=True               # 要不要打乱数据 (打乱比较好)
+    dataset=torch_dataset,  # torch TensorDataset format
+    batch_size=BATCH_SIZE,  # mini batch size
+    shuffle=True  # 要不要打乱数据 (打乱比较好)
 )
 
 
@@ -132,22 +134,27 @@ class RNN(nn.Module):
         for vector in x:
             for item in vector:
                 if new_vector is None:
-                    new_vector = torch.cat((self.car_embeds(torch.LongTensor([item[0].item()]))[0], self.region_embeds(torch.LongTensor([item[1].item()]))[0]))
-                    new_vector = torch.cat((new_vector, self.poi_embeds(torch.LongTensor([item[2].item()]))[0]))
+                    new_vector = torch.cat((self.car_embeds(torch.cuda.LongTensor([item[0].item()]))[0],
+                                            self.region_embeds(torch.cuda.LongTensor([item[1].item()]))[0]))
+                    new_vector = torch.cat((new_vector, self.poi_embeds(torch.cuda.LongTensor([item[2].item()]))[0]))
                 else:
-                    new_vector = torch.cat((new_vector, self.car_embeds(torch.LongTensor([item[0].item()]))[0]))
-                    new_vector = torch.cat((new_vector, self.region_embeds(torch.LongTensor([item[1].item()]))[0]))
-                    new_vector = torch.cat((new_vector, self.poi_embeds(torch.LongTensor([item[2].item()]))[0]))
+                    new_vector = torch.cat((new_vector, self.car_embeds(torch.cuda.LongTensor([item[0].item()]))[0]))
+                    new_vector = torch.cat((new_vector, self.region_embeds(torch.cuda.LongTensor([item[1].item()]))[0]))
+                    new_vector = torch.cat((new_vector, self.poi_embeds(torch.cuda.LongTensor([item[2].item()]))[0]))
         x = new_vector.view(-1, 10, 30)
+        if gpu_avaliable:
+            x = x.cuda()
         r_out, (h_n, h_c) = self.rnn(x, None)  # None represents zero initial hidden state
 
         # choose r_out at the last time step
         out = self.out(r_out[:, -1, :])
-        out = F.softmax(out, 1)
+        # out = F.softmax(out, 1)
         return out
 
 
 rnn = RNN()
+if gpu_avaliable:
+    rnn.cuda()
 print(rnn)
 
 optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)  # optimize all cnn parameters
@@ -156,6 +163,10 @@ loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
 # training and testing
 for epoch in range(EPOCH):
     for step, (b_x, b_y) in enumerate(loader):  # gives batch data
+        if gpu_avaliable:
+            b_x = b_x.cuda()
+            b_y = b_y.cuda()
+
         b_x = b_x.view(-1, 10, 3)  # reshape x to (batch, time_step, input_size)
 
         output = rnn(b_x)  # rnn output
@@ -166,12 +177,18 @@ for epoch in range(EPOCH):
 
         if step % 50 == 0:
             test_output = rnn(test_data)  # (samples, time_step, input_size)
-            pred_y = torch.max(test_output, 1)[1].data.numpy()
-            accuracy = float((pred_y == test_labels).astype(int).sum()) / float(test_labels.size)
-            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
+            if gpu_avaliable:
+                pred_y = torch.max(test_output, 1)[1].cuda().data
+            else:
+                pred_y = torch.max(test_output, 1)[1].data.numpy()
+            accuracy = torch.sum(pred_y == test_labels).type(torch.FloatTensor) / test_labels.size(0)
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.cpu().numpy(), '| test accuracy: %.2f' % accuracy)
 
 # print 10 predictions from test data
 test_output = rnn(test_data[:10].view(-1, 10, 3))
-pred_y = torch.max(test_output, 1)[1].data.numpy()
+if gpu_avaliable:
+    pred_y = torch.max(test_output, 1)[1].cuda().data
+else:
+    pred_y = torch.max(test_output, 1)[1].data.numpy()
 print(pred_y, 'prediction number')
 print(test_labels[:10], 'real number')
