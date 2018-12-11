9cc0a0c50cf24af39d67eb669689f6335bea7686
haoc_dp <haoc_dp@163.com>
2018-12-11 21:02:16 +0800

    update

--- trajectory_handle_2014/raw_data_handle/prehandle.py
+++ trajectory_handle_2014/raw_data_handle/prehandle.py
@@ -3,6 +3,8 @@
 
 import sys
 sys.path.append("/root/trajectory_handle/")
+reload(sys) # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
+sys.setdefaultencoding('utf-8')
 
 from concurrent.futures import ThreadPoolExecutor
 import os
