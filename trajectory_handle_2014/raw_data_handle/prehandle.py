bb6a599237137908806ed715f6993185a1ab2bc4
haoc_dp <haoc_dp@163.com>
2018-12-14 17:01:39 +0800

    update

--- trajectory_handle_2014/raw_data_handle/prehandle.py
+++ trajectory_handle_2014/raw_data_handle/prehandle.py
@@ -135,9 +135,9 @@ def write_file(key, plateNumber_dict, path):
     if key == '\n' or key is '\n':
         return
 
-    file = open(path + "/" + key + ".txt", 'w', encoding='gbk')
+    file = open(path + "/" + key + ".txt", 'w', encoding='utf-8')
     for line in plateNumber_dict.get(key):
-        file.write(line.encode('gbk'))
+        file.write(line.encode('utf-8'))
     file.close()
 
 
