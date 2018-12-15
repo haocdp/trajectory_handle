4527181e53a75ca0b3b14fd9991609a26c979ae5
haoc_dp <haoc_dp@163.com>
2018-12-15 19:00:11 +0800

    update

--- trajectory_handle_2014/raw_data_handle/run.sh
+++ trajectory_handle_2014/raw_data_handle/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python3 trajectory_handle_2014/raw_data_handle/prehandle.py > logs/1.log & \
-python3 trajectory_handle_2014/raw_data_handle/prehandle2.py > logs/2.log & \
-python3 trajectory_handle_2014/raw_data_handle/prehandle3.py > logs/3.log & \
-python3 trajectory_handle_2014/raw_data_handle/prehandle4.py > logs/4.log & \
-python3 trajectory_handle_2014/raw_data_handle/prehandle5.py > logs/5.log & \
-python3 trajectory_handle_2014/raw_data_handle/prehandle6.py > logs/6.log & \
-python3 trajectory_handle_2014/raw_data_handle/prehandle7.py > logs/7.log
+python3 trajectory_handle_2014/raw_data_handle/prehandle.py > /root/logs/1.log & \
+python3 trajectory_handle_2014/raw_data_handle/prehandle2.py > /root/logs/2.log & \
+python3 trajectory_handle_2014/raw_data_handle/prehandle3.py > /root/logs/3.log & \
+python3 trajectory_handle_2014/raw_data_handle/prehandle4.py > /root/logs/4.log & \
+python3 trajectory_handle_2014/raw_data_handle/prehandle5.py > /root/logs/5.log & \
+python3 trajectory_handle_2014/raw_data_handle/prehandle6.py > /root/logs/6.log & \
+python3 trajectory_handle_2014/raw_data_handle/prehandle7.py > /root/logs/7.log
