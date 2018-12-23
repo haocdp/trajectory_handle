28ed80ce1788f4eaf336a54a07b0c9efa56c7bd0
haoc_dp <haoc_dp@163.com>
2018-12-23 15:54:26 +0800

    update

--- destination_prediction/with_time/lstm_prediction_qian_10dian_with_time.py
+++ destination_prediction/with_time/lstm_prediction_qian_10dian_with_time.py
@@ -33,7 +33,7 @@ LAYER_NUM = 2
 
 linux_path = "/root/taxiData"
 windows_path = "F:/FCD data"
-base_path = windows_path
+base_path = linux_path
 
 labels = list(np.load(base_path + "/cluster/destination_labels.npy"))
 # label个数
@@ -41,31 +41,31 @@ label_size = len(set(labels))
 elogger = logger.Logger("lstm_prediction")
 
 def load_data():
-    # filepath1 = base_path + "/trajectory/2014-10-20/trajectory_2014-10-20result_npy.npy"
-    filepath1 = base_path + "/trajectory/allday/youke_0_result_npy.npy"
-    # filepath2 = base_path + "/trajectory/2014-10-21/trajectory_2014-10-21result_npy.npy"
-    # filepath3 = base_path + "/trajectory/2014-10-22/trajectory_2014-10-22result_npy.npy"
-    # filepath4 = base_path + "/trajectory/2014-10-23/trajectory_2014-10-23result_npy.npy"
-    # filepath5 = base_path + "/trajectory/2014-10-24/trajectory_2014-10-24result_npy.npy"
-    # filepath6 = base_path + "/trajectory/2014-10-25/trajectory_2014-10-25result_npy.npy"
-    # filepath7 = base_path + "/trajectory/2014-10-26/trajectory_2014-10-26result_npy.npy"
+    filepath1 = base_path + "/trajectory/2014-10-20/trajectory_2014-10-20result_npy.npy"
+    # filepath1 = base_path + "/trajectory/allday/youke_0_result_npy.npy"
+    filepath2 = base_path + "/trajectory/2014-10-21/trajectory_2014-10-21result_npy.npy"
+    filepath3 = base_path + "/trajectory/2014-10-22/trajectory_2014-10-22result_npy.npy"
+    filepath4 = base_path + "/trajectory/2014-10-23/trajectory_2014-10-23result_npy.npy"
+    filepath5 = base_path + "/trajectory/2014-10-24/trajectory_2014-10-24result_npy.npy"
+    filepath6 = base_path + "/trajectory/2014-10-25/trajectory_2014-10-25result_npy.npy"
+    filepath7 = base_path + "/trajectory/2014-10-26/trajectory_2014-10-26result_npy.npy"
 
     trajectories1 = list(np.load(filepath1))
-    # trajectories2 = list(np.load(filepath2))
-    # trajectories3 = list(np.load(filepath3))
-    # trajectories4 = list(np.load(filepath4))
-    # trajectories5 = list(np.load(filepath5))
-    # trajectories6 = list(np.load(filepath6))
-    # trajectories7 = list(np.load(filepath7))
+    trajectories2 = list(np.load(filepath2))
+    trajectories3 = list(np.load(filepath3))
+    trajectories4 = list(np.load(filepath4))
+    trajectories5 = list(np.load(filepath5))
+    trajectories6 = list(np.load(filepath6))
+    trajectories7 = list(np.load(filepath7))
 
     all_trajectories = []
     all_trajectories.extend(trajectories1)
-    # all_trajectories.extend(trajectories2)
-    # all_trajectories.extend(trajectories3)
-    # all_trajectories.extend(trajectories4)
-    # all_trajectories.extend(trajectories5)
-    # all_trajectories.extend(trajectories6)
-    # all_trajectories.extend(trajectories7)
+    all_trajectories.extend(trajectories2)
+    all_trajectories.extend(trajectories3)
+    all_trajectories.extend(trajectories4)
+    all_trajectories.extend(trajectories5)
+    all_trajectories.extend(trajectories6)
+    all_trajectories.extend(trajectories7)
 
     # 打乱
     random.shuffle(all_trajectories)
