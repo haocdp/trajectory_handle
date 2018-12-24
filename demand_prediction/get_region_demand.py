f6ce82deb695384654fef618106363d6510bfecc
haoc_dp <haoc_dp@163.com>
2018-12-24 14:27:37 +0800

    update

---
+++ demand_prediction/get_region_demand.py
@@ -0,0 +1,82 @@
+"""
+训练数据：（car_id, region_id, poi_id, week_day, time_slot）
+"""
+import os
+import sys
+if sys.platform == 'linux':
+    sys.path.append('/root/trajectory_handle/')
+
+import torch
+from torch import nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.utils.data as Data
+import torch.nn.functional as F
+import random
+import logger
+from destination_prediction.evaluation.Evaluate import Evaluate
+from demand_prediction.region2matrix import get_region_matrix
+
+# torch.manual_seed(1)    # reproducible
+
+linux_path = "/root/taxiData"
+windows_path = "F:/TaxiData"
+base_path = linux_path
+
+
+def load_data():
+    filepath1 = base_path + "/trajectory/2014-10-20/trajectory_2014-10-20result_npy.npy"
+    # filepath1 = base_path + "/trajectory/allday/youke_0_result_npy.npy"
+    filepath2 = base_path + "/trajectory/2014-10-21/trajectory_2014-10-21result_npy.npy"
+    filepath3 = base_path + "/trajectory/2014-10-22/trajectory_2014-10-22result_npy.npy"
+    filepath4 = base_path + "/trajectory/2014-10-23/trajectory_2014-10-23result_npy.npy"
+    filepath5 = base_path + "/trajectory/2014-10-24/trajectory_2014-10-24result_npy.npy"
+    filepath6 = base_path + "/trajectory/2014-10-25/trajectory_2014-10-25result_npy.npy"
+    filepath7 = base_path + "/trajectory/2014-10-26/trajectory_2014-10-26result_npy.npy"
+
+    trajectories1 = list(np.load(filepath1))
+    trajectories2 = list(np.load(filepath2))
+    trajectories3 = list(np.load(filepath3))
+    trajectories4 = list(np.load(filepath4))
+    trajectories5 = list(np.load(filepath5))
+    trajectories6 = list(np.load(filepath6))
+    trajectories7 = list(np.load(filepath7))
+
+    all_trajectories = []
+    all_trajectories.extend(trajectories1)
+    all_trajectories.extend(trajectories2)
+    all_trajectories.extend(trajectories3)
+    all_trajectories.extend(trajectories4)
+    all_trajectories.extend(trajectories5)
+    all_trajectories.extend(trajectories6)
+    all_trajectories.extend(trajectories7)
+
+    # 打乱
+    # random.shuffle(all_trajectories)
+
+    print("all trajectories num : {}".format(len(all_trajectories)))
+    # count = len(all_trajectories) * 0.8
+
+    region_demand = []
+    for i in range(28):
+        region_demand.append([])
+        for j in range(60):
+            region_demand[i].append([])
+            for k in range(48):
+                region_demand[i][j].append([-1, -1, -1, -1, -1, -1, -1])
+
+    all_grid, region_to_ix = get_region_matrix()
+    for trajectory, label, weekday, time_slot in all_trajectories:
+        time_slot = int(time_slot / 30)
+        i, j = region_to_ix[int(trajectory[0][-2])]
+        if region_demand[i][j][time_slot][weekday] == -1:
+            region_demand[i][j][time_slot][weekday] = 1
+        else:
+            region_demand[i][j][time_slot][weekday] += 1
+
+    print(region_demand)
+    np.save(base_path + "/demand/region_demand", region_demand)
+
+load_data()
---
+++ demand_prediction/region2matrix.py
@@ -0,0 +1,52 @@
+import redis
+from decimal import *
+import numpy as np
+# 从redis中读取城市网格，并将对应城市区域对应到28 x 60的矩阵中
+
+linux_path = "/root/taxiData"
+windows_path = "F:/TaxiData"
+base_path = windows_path
+
+
+def get_region_matrix():
+    r = redis.Redis(host='127.0.0.1', port=6379, charset='utf-8')
+    taz_grids = {}
+    for i in range(918):
+        taz_grid = str(r.get('taz_grid_' + str(i)), 'utf-8')
+        xmin, xmax, ymin, ymax = list(map(Decimal, taz_grid.split(";")))
+        taz_grids[i] = (xmin, xmax, ymin, ymax)
+
+    # print(taz_grids)
+
+    all_grid = []
+    region_to_ix = {}
+    for i in range(28):
+        all_grid.append([])
+        for j in range(60):
+            all_grid[i].append(-1)
+
+    xmin = Decimal('180.00000')
+    ymin = Decimal('90.00000')
+    for key in taz_grids.keys():
+        region_xmin = taz_grids.get(key)[0]
+        region_ymin = taz_grids.get(key)[2]
+        if xmin > region_xmin:
+            xmin = region_xmin
+        if ymin > region_ymin:
+            ymin = region_ymin
+
+    for key in taz_grids.keys():
+        if key == 819:
+            print('test')
+        region_xmin = taz_grids.get(key)[0]
+        region_ymin = taz_grids.get(key)[2]
+        i = int(float(region_xmin - xmin) / 0.015)
+        j = 27 - int(float(region_ymin - ymin) / 0.015)
+        print("i = {}, j = {}".format(i, j))
+        all_grid[j][i] = key
+        region_to_ix[key] = (j, i)
+
+    np.save(base_path + "/demand/region_matrix", all_grid)
+    np.save(base_path + "/demand/region_to_ix", region_to_ix)
+    # print(all_grid)
+    return all_grid, region_to_ix
