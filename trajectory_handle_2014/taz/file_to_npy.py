e6b0b06ee05a6feb11ec366d71d6b8357bc31e4e
haoc_dp <haoc_dp@163.com>
2018-12-31 16:52:36 +0800

    update

--- trajectory_handle_2014/taz/file_to_npy.py
+++ trajectory_handle_2014/taz/file_to_npy.py
@@ -2,11 +2,21 @@ import numpy as np
 from ast import literal_eval
 
 # file_path = "F:/TaxiData/trajectory_taz/2014-10-26/trajectory_2014-10-26_result"
-linux_file_path = "/root/taxiData/trajectory/2014-10-20/trajectory_2014-10-20_result"
+linux_file_path2 = "/root/taxiData/trajectory/2014-10-21/trajectory_2014-10-21_result"
+linux_file_path3 = "/root/taxiData/trajectory/2014-10-22/trajectory_2014-10-22_result"
+linux_file_path4 = "/root/taxiData/trajectory/2014-10-23/trajectory_2014-10-23_result"
+linux_file_path5 = "/root/taxiData/trajectory/2014-10-24/trajectory_2014-10-24_result"
+linux_file_path6 = "/root/taxiData/trajectory/2014-10-25/trajectory_2014-10-25_result"
+linux_file_path7 = "/root/taxiData/trajectory/2014-10-26/trajectory_2014-10-26_result"
 # save_path = "F:/TaxiData/trajectory_taz/2014-10-26/trajectory_2014-10-26result_npy"
-linux_save_path = "/root/taxiData/trajectory/2014-10-20/trajectory_2014-10-20_result_npy"
+linux_save_path2 = "/root/taxiData/trajectory/2014-10-21/trajectory_2014-10-21_result_npy"
+linux_save_path3 = "/root/taxiData/trajectory/2014-10-22/trajectory_2014-10-22_result_npy"
+linux_save_path4 = "/root/taxiData/trajectory/2014-10-23/trajectory_2014-10-23_result_npy"
+linux_save_path5 = "/root/taxiData/trajectory/2014-10-24/trajectory_2014-10-24_result_npy"
+linux_save_path6 = "/root/taxiData/trajectory/2014-10-25/trajectory_2014-10-25_result_npy"
+linux_save_path7 = "/root/taxiData/trajectory/2014-10-26/trajectory_2014-10-26_result_npy"
 
-file = open(linux_file_path, 'r')
+file = open(linux_file_path2, 'r')
 lines = file.readlines()
 
 trajectories = []
@@ -14,4 +24,56 @@ for line in lines:
     trajectory, cluster, weekday, time_slot = line.strip().split(";")
     trajectory = literal_eval(trajectory)
     trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
-np.save(linux_save_path, trajectories)
+np.save(linux_save_path2, trajectories)
+
+
+file = open(linux_file_path3, 'r')
+lines = file.readlines()
+
+trajectories = []
+for line in lines:
+    trajectory, cluster, weekday, time_slot = line.strip().split(";")
+    trajectory = literal_eval(trajectory)
+    trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
+np.save(linux_save_path3, trajectories)
+
+
+file = open(linux_file_path4, 'r')
+lines = file.readlines()
+
+trajectories = []
+for line in lines:
+    trajectory, cluster, weekday, time_slot = line.strip().split(";")
+    trajectory = literal_eval(trajectory)
+    trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
+np.save(linux_save_path4, trajectories)
+
+file = open(linux_file_path5, 'r')
+lines = file.readlines()
+
+trajectories = []
+for line in lines:
+    trajectory, cluster, weekday, time_slot = line.strip().split(";")
+    trajectory = literal_eval(trajectory)
+    trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
+np.save(linux_save_path5, trajectories)
+
+file = open(linux_file_path6, 'r')
+lines = file.readlines()
+
+trajectories = []
+for line in lines:
+    trajectory, cluster, weekday, time_slot = line.strip().split(";")
+    trajectory = literal_eval(trajectory)
+    trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
+np.save(linux_save_path6, trajectories)
+
+file = open(linux_file_path7, 'r')
+lines = file.readlines()
+
+trajectories = []
+for line in lines:
+    trajectory, cluster, weekday, time_slot = line.strip().split(";")
+    trajectory = literal_eval(trajectory)
+    trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
+np.save(linux_save_path7, trajectories)
