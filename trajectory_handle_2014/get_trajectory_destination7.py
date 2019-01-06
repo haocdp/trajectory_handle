fca5aff0433126235c00eb212b22baa129500ffa
haoc_dp <haoc_dp@163.com>
2019-01-06 17:48:05 +0800

    update

--- trajectory_handle_2014/get_trajectory_destination7.py
+++ trajectory_handle_2014/get_trajectory_destination7.py
@@ -12,7 +12,7 @@ import time
 
 windows_path = "F:/TaxiData"
 linux_path = "/root/taxiData"
-base_path = windows_path
+base_path = linux_path
 
 file_path = "2014-10-26"
 dir_path = "/trajectory_without_filter/"
