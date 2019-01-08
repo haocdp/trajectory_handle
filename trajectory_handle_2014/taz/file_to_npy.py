c7f56055bc136e6f49477f5c2867c139dd65dd8a
haoc_dp <haoc_dp@163.com>
2019-01-08 18:16:10 +0800

    update

--- trajectory_handle_2014/taz/file_to_npy.py
+++ trajectory_handle_2014/taz/file_to_npy.py
@@ -1,20 +1,31 @@
 import numpy as np
 from ast import literal_eval
 
-# file_path = "F:/TaxiData/trajectory_taz/2014-10-26/trajectory_2014-10-26_result"
-linux_file_path2 = "/root/taxiData/trajectory_without_filter/2014-10-24/trajectory_2014-10-24_result"
-# linux_file_path3 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-22/trajectory_2014-10-22_result"
-# linux_file_path4 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-23/trajectory_2014-10-23_result"
-# linux_file_path5 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-24/trajectory_2014-10-24_result"
-# linux_file_path6 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-25/trajectory_2014-10-25_result"
-# linux_file_path7 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-26/trajectory_2014-10-26_result"
-# save_path = "F:/TaxiData/trajectory_taz/2014-10-26/trajectory_2014-10-26result_npy"
-linux_save_path2 = "/root/taxiData/trajectory_without_filter/2014-10-24/trajectory_2014-10-24result_npy"
-# linux_save_path3 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-22/trajectory_2014-10-22_result_npy"
-# linux_save_path4 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-23/trajectory_2014-10-23_result_npy"
-# linux_save_path5 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-24/trajectory_2014-10-24_result_npy"
-# linux_save_path6 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-25/trajectory_2014-10-25_result_npy"
-# linux_save_path7 = "D:\haoc\data\TaxiData\\trajectory_taz_without_filter/2014-10-26/trajectory_2014-10-26_result_npy"
+linux_file_path1 = "/root/taxiData/trajectory_without_filter/2014-10-20/trajectory_2014-10-20_result"
+linux_file_path2 = "/root/taxiData/trajectory_without_filter/2014-10-21/trajectory_2014-10-21_result"
+linux_file_path3 = "/root/taxiData/trajectory_without_filter/2014-10-22/trajectory_2014-10-22_result"
+linux_file_path4 = "/root/taxiData/trajectory_without_filter/2014-10-23/trajectory_2014-10-23_result"
+linux_file_path5 = "/root/taxiData/trajectory_without_filter/2014-10-24/trajectory_2014-10-24_result"
+linux_file_path6 = "/root/taxiData/trajectory_without_filter/2014-10-25/trajectory_2014-10-25_result"
+linux_file_path7 = "/root/taxiData/trajectory_without_filter/2014-10-26/trajectory_2014-10-26_result"
+
+linux_save_path1 = "/root/taxiData/trajectory_without_filter/2014-10-20/trajectory_2014-10-20_result_npy"
+linux_save_path2 = "/root/taxiData/trajectory_without_filter/2014-10-21/trajectory_2014-10-21_result_npy"
+linux_save_path3 = "/root/taxiData/trajectory_without_filter/2014-10-22/trajectory_2014-10-22_result_npy"
+linux_save_path4 = "/root/taxiData/trajectory_without_filter/2014-10-23/trajectory_2014-10-23_result_npy"
+linux_save_path5 = "/root/taxiData/trajectory_without_filter/2014-10-24/trajectory_2014-10-24_result_npy"
+linux_save_path6 = "/root/taxiData/trajectory_without_filter/2014-10-25/trajectory_2014-10-25_result_npy"
+linux_save_path7 = "/root/taxiData/trajectory_without_filter/2014-10-26/trajectory_2014-10-26_result_npy"
+
+file = open(linux_file_path1, 'r')
+lines = file.readlines()
+
+trajectories = []
+for line in lines:
+    trajectory, cluster, weekday, time_slot = line.strip().split(";")
+    trajectory = literal_eval(trajectory)
+    trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
+np.save(linux_save_path1, trajectories)
 
 file = open(linux_file_path2, 'r')
 lines = file.readlines()
@@ -25,9 +36,6 @@ for line in lines:
     trajectory = literal_eval(trajectory)
     trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
 np.save(linux_save_path2, trajectories)
-
-"""
-
 
 file = open(linux_file_path3, 'r')
 lines = file.readlines()
@@ -79,4 +87,3 @@ for line in lines:
     trajectory = literal_eval(trajectory)
     trajectories.append((trajectory, int(cluster), int(weekday), int(time_slot)))
 np.save(linux_save_path7, trajectories)
-"""
