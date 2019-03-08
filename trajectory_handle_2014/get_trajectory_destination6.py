e779deafa9807528f5a22c6c1b204d5995e2ec4d
haoc_dp <1134675798@qq.com>
2019-03-08 10:18:45 +0800

    update

--- trajectory_handle_2014/get_trajectory_destination6.py
+++ trajectory_handle_2014/get_trajectory_destination6.py
@@ -12,7 +12,7 @@ import time
 
 windows_path = "K:/毕业论文/TaxiData"
 linux_path = "/root/taxiData"
-base_path = windows_path
+base_path = linux_path
 
 file_path = "2014-11-01"
 dir_path = "/trajectory_without_filter/"
